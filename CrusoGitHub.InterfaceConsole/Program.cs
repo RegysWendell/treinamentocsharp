69ae80295f30e6a22e1616a766de871ea750657d
RegysWendell <regys.sousa@aluno.senai.br>
2022-07-04 10:54:10 -0300

    teste de commit

---
+++ CrusoGitHub.InterfaceConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace CrusoGitHub.InterfaceConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alo Mundo!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
